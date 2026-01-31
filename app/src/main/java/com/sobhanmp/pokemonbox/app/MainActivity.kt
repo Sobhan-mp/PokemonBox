d30e4517514ab92a59b79e92811e9715435bbc94
Sobhan Mohammadpour <sobhanmohamadpour@gmail.com>
2026-01-31 18:36:57 +0100

    Fix: Scaffold us replaced with a box

--- app/src/main/java/com/sobhanmp/pokemonbox/app/MainActivity.kt
+++ app/src/main/java/com/sobhanmp/pokemonbox/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PokemonBOXTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Box(modifier = Modifier.fillMaxSize()) {
 
                     val backStack = remember { mutableStateListOf<NavKey>(Home) }
                     NavDisplay(
